5c726de8db7fbf02d89227b20afec01fd9cae146
jakzo <jack@jf.id.au>
2022-07-03 21:10:39 +1000

    keep prefs

--- src/PluginResetSave.cs
+++ src/PluginResetSave.cs
@@ -4,23 +4,64 @@ namespace SpeedrunTools
 {
   class PluginResetSave : Plugin
   {
-    private const int SCENE_MENU_IDX = 1;
     private static int s_currentSceneIdx;
 
     public readonly Hotkey HotkeyReset = new Hotkey()
     {
-      Predicate = (cl, cr) => cl.GetAButton() && cl.GetBButton(),
+      Predicate = (cl, cr) =>
+        s_currentSceneIdx == Utils.SCENE_MENU_IDX &&
+        cl.GetAButton() && cl.GetBButton() &&
+        cr.GetAButton() && cr.GetBButton(),
       Handler = () =>
       {
-        if (s_currentSceneIdx != SCENE_MENU_IDX)
-        {
-          MelonLogger.Warning("Can only reset save when in menu. Not resetting save.");
-          return;
-        }
-
         MelonLogger.Msg("Resetting save");
-        UnityEngine.Object.FindObjectOfType<Data_Manager>().DATA_DEFAULT();
-        StressLevelZero.Utilities.BoneworksSceneManager.ReloadScene();
+        var dataManager = UnityEngine.Object.FindObjectOfType<Data_Manager>();
+        var oldData = dataManager.data_player;
+        dataManager.DATA_DEFAULT_ALL();
+
+        // Copy previous preferences
+        var newData = dataManager.data_player;
+        newData.additionalLighting = oldData.additionalLighting;
+        newData.aliasing = oldData.aliasing;
+        newData.ambientOcclusion = oldData.ambientOcclusion;
+        newData.audio_GlobalVolume = oldData.audio_GlobalVolume;
+        newData.audio_Music = oldData.audio_Music;
+        newData.audio_SFX = oldData.audio_SFX;
+        newData.beltRightSide = oldData.beltRightSide;
+        newData.bloom = oldData.bloom;
+        newData.fisheye = oldData.fisheye;
+        newData.fisheyeLocation = oldData.fisheyeLocation;
+        newData.isAdaptiveOn = oldData.isAdaptiveOn;
+        newData.isInverted = oldData.isInverted;
+        newData.isRightHanded = oldData.isRightHanded;
+        newData.joySensitivityNew = oldData.joySensitivityNew;
+        newData.language = oldData.language;
+        newData.loco_Curve = oldData.loco_Curve;
+        newData.loco_DegreesPerSnap = oldData.loco_DegreesPerSnap;
+        newData.loco_Direction = oldData.loco_Direction;
+        newData.loco_SnapDegreesPerFrame = oldData.loco_SnapDegreesPerFrame;
+        newData.mod_Haptic = oldData.mod_Haptic;
+        newData.motionBlur = oldData.motionBlur;
+        newData.mouseSensitivityNew = oldData.mouseSensitivityNew;
+        newData.offset_Floor = oldData.offset_Floor;
+        newData.offset_Sitting = oldData.offset_Sitting;
+        newData.physicsUpdateRate = oldData.physicsUpdateRate;
+        newData.player_Height = oldData.player_Height;
+        newData.player_Name = oldData.player_Name;
+        newData.playIntroCheck = oldData.playIntroCheck;
+        newData.playMode = oldData.playMode; // safe?
+        newData.profile_Name = oldData.profile_Name;
+        newData.quality = oldData.quality;
+        newData.resX = oldData.resX;
+        newData.resY = oldData.resY;
+        newData.shadowQuality = oldData.shadowQuality;
+        newData.SnapEnabled = oldData.SnapEnabled;
+        newData.standing = oldData.standing;
+        newData.subtitlesOn = oldData.subtitlesOn;
+        newData.TextureResolution = oldData.TextureResolution;
+        newData.VirtualCrouching = oldData.VirtualCrouching;
+
+        dataManager.RELOADSCENE();
       }
     };
 

c5675fa4a3d86e8842f92f6abb662fac3315f300
jakzo <jack@jf.id.au>
2022-10-21 22:54:50 +1100

    add icon

---
+++ snippets/BonelabShaders.cs
@@ -0,0 +1,159 @@
+/*
+
+Shader list:
+Hidden/Internal-MotionVectors
+Sprites/Default
+UI/Default
+Hidden/Universal Render Pipeline/Blit Alpha Premultiply
+Hidden/BlitCopy
+Hidden/BlitCopyWithDepth
+Hidden/Internal-GUITextureClip
+Hidden/Internal-GUITexture
+Hidden/Internal-GUIRoundedRect
+Hidden/Internal-GUIRoundedRectWithColorPerBorder
+Sprites/Mask
+Hidden/InternalErrorShader
+Hidden/Internal-Loading
+Hidden/Internal-Loading
+GUI/Text Shader
+Standard
+Legacy Shaders/VertexLit
+Hidden/Universal Render Pipeline/Blit
+Hidden/Universal Render Pipeline/CopyDepth
+Hidden/Universal Render Pipeline/LutBuilderHdr
+Hidden/Universal Render Pipeline/LutBuilderLdr
+Hidden/Universal Render Pipeline/Sampling
+Hidden/Universal Render Pipeline/ScreenSpaceShadows
+Hidden/Universal Render Pipeline/UberPost
+Hidden/Universal Render Pipeline/HBAO
+Hidden/Universal Render Pipeline/Debug/DebugReplacement
+Hidden/Universal Render Pipeline/StencilDeferred
+Hidden/Universal Render Pipeline/FallbackError
+Hidden/Universal Render Pipeline/MaterialError
+Hidden/Universal/CoreBlit
+Hidden/Universal/CoreBlitColorAndDepth
+Hidden/kMotion/CameraMotionVectors
+Hidden/kMotion/ObjectMotionVectors
+Hidden/Universal Render Pipeline/ScreenSpaceAmbientOcclusion
+Hidden/Universal Render Pipeline/Stop NaN
+Hidden/Universal Render Pipeline/SubpixelMorphologicalAntialiasing
+Hidden/Universal Render Pipeline/GaussianDepthOfField
+Hidden/Universal Render Pipeline/BokehDepthOfField
+Hidden/Universal Render Pipeline/CameraMotionBlur
+Hidden/Universal Render Pipeline/PaniniProjection
+Hidden/Universal Render Pipeline/Bloom
+Hidden/Universal Render Pipeline/LensFlareDataDriven
+Hidden/Universal Render Pipeline/Scaling Setup
+Hidden/Universal Render Pipeline/Edge Adaptive Spatial Upsampling
+Hidden/Universal Render Pipeline/FinalPost
+Hidden/Universal Render Pipeline/XR/XROcclusionMesh
+Hidden/Universal Render Pipeline/XR/XRMirrorView
+Hidden/MK/Glow/MKGlowSM40
+SLZ/Icon Billboard
+Hidden/MK/Glow/MKGlowSM35
+TextMeshPro/Sprite
+Hidden/MK/Glow/MKGlowSM25
+Hidden/MK/Glow/MKGlowSM20
+Hidden/MK/Glow/SelectiveRender
+TextMeshPro/Mobile/Distance Field
+Hidden/MK/Glow/MKGlowSM30
+Hidden/Universal Render Pipeline/CopyDepthToColor
+Universal Render Pipeline/Unlit
+Universal Render Pipeline/Lit (PBR Workflow)
+Hidden/Universal Render Pipeline/FallbackError
+TextMeshPro/Distance Field
+TextMeshPro/Mobile/Distance Field
+Sprites/Default
+DefaultUI
+UI/Default Font
+UI/Unlit/Text
+UI/Default
+UI/Unlit/Transparent
+SLZ/LitMAS/LitMAS Opaque
+Lux URP/Human/Skin
+Custom/HighlightAlt
+SLZ/Fuzz Shell
+SLZ/Additive HDR
+SLZ/Highlighter
+Universal Render Pipeline/Particles/Lit (Particles)
+SLZ/AdditiveWithDistortion
+SLZ/LitMAS/LitMAS Triplanar
+SLZ/Gacha
+Lux URP/Human/Hair Blend
+SLZ/Fuzz Sheen
+SLZ/LitMAS/LitMAS Vertex Color AO
+SLZ/LitMAS/LitMAS Posespace
+Universal Render Pipeline/Particles/Unlit (Particles)
+SLZ/Hologram Depth fade
+Unlit/URP Unlit Color
+SLZ/TextureRampEmissive
+Shadergraph Fixed/EyeAdvanced_LOD0_URP
+SLZ/Mod2x
+Universal Render Pipeline/Particles/Simple Lit
+SLZ/ParticleStar
+SLZ/SpiderChart
+SLZ/Simple Geo Skybox
+SLZ/OpaqueMotion
+StressLevelZero/Lines/Colored Blended
+SLZ/Fuzz Sheen Terrain First-Pass
+Lux URP/Human/Hair
+Baked Shadergraphs/TMP_SDF-URP Lit
+Standard
+Legacy Shaders/VertexLit
+SLZ/ConstelationLines
+Hidden/TerrainEngine/Details/UniversalPipeline/BillboardWavingDoublePass
+Hidden/TerrainEngine/CameraFacingBillboardTree
+SLZ/Holographic Wall Simple
+Hidden/TerrainEngine/Details/UniversalPipeline/Vertexlit
+Hidden/kMotion/ObjectMotionVectors
+Hidden/Universal Render Pipeline/Blit
+Hidden/Universal Render Pipeline/XR/XRMirrorView
+Hidden/Universal Render Pipeline/XR/XROcclusionMesh
+Hidden/Universal Render Pipeline/FinalPost
+Hidden/Universal Render Pipeline/GaussianDepthOfField
+Hidden/Universal Render Pipeline/BokehDepthOfField
+Hidden/Universal Render Pipeline/LutBuilderHdr
+Hidden/Universal Render Pipeline/Scaling Setup
+Hidden/Universal Render Pipeline/LutBuilderLdr
+Hidden/Universal Render Pipeline/Stop NaN
+Hidden/Universal Render Pipeline/PaniniProjection
+Hidden/Universal Render Pipeline/UberPost
+Hidden/Universal Render Pipeline/Bloom
+Hidden/Universal Render Pipeline/LensFlareDataDriven
+Hidden/Universal Render Pipeline/CameraMotionBlur
+Hidden/Universal Render Pipeline/Edge Adaptive Spatial Upsampling
+Hidden/Universal Render Pipeline/SubpixelMorphologicalAntialiasing
+Hidden/Universal Render Pipeline/ScreenSpaceShadows
+Hidden/Universal Render Pipeline/StencilDeferred
+Hidden/Universal Render Pipeline/CopyDepthToColor
+Hidden/Universal Render Pipeline/Debug/DebugReplacement
+Hidden/Universal Render Pipeline/Sampling
+Hidden/Universal/CoreBlitColorAndDepth
+Hidden/kMotion/CameraMotionVectors
+Hidden/Universal Render Pipeline/CopyDepth
+Hidden/Universal Render Pipeline/MaterialError
+Hidden/Universal Render Pipeline/ScreenSpaceAmbientOcclusion
+Hidden/TerrainEngine/Details/UniversalPipeline/WavingDoublePass
+Hidden/Universal/CoreBlit
+Hidden/Universal Render Pipeline/HBAO
+Hidden/Shader Graph/FallbackError
+Hidden/Shader Graph/FallbackError
+SLZ/Vignette
+Hidden/Nature/Terrain/Utilities
+Hidden/TerrainEngine/HeightBlitCopy
+Hidden/TerrainEngine/PaintHeight
+Hidden/TerrainEngine/CrossBlendNeighbors
+Hidden/Nature/Tree Creator Albedo Rendertex
+Hidden/Nature/Tree Creator Normal Rendertex
+Hidden/Nature/Tree Soft Occlusion Bark Rendertex
+Hidden/TerrainEngine/GenerateNormalmap
+Hidden/TerrainEngine/Splatmap/Standard-BaseGen
+Hidden/TerrainEngine/TerrainLayerUtils
+Hidden/Nature/Tree Soft Occlusion Leaves Rendertex
+Skybox/Cubemap
+Hidden/TerrainEngine/BillboardTree
+SLZ/Particle/Motion Vector Billboard Correct Shadows
+SLZ/Lit MAS Transparent
+SLZ/Anime
+
+*/

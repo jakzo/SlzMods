3d2150967df0a3e8505f6e512a68bc4c89e9c7a3
jakzo <jack@jf.id.au>
2024-05-09 00:10:08 +1000

    pancake stuff

--- projects/Bonelab/PancakeMode/src/Mod.cs
+++ projects/Bonelab/PancakeMode/src/Mod.cs
@@ -1,10 +1,13 @@
 using MelonLoader;
 using System;
 using System.Collections.Generic;
+using SLZ;
 using SLZ.Marrow.Warehouse;
 using SLZ.Rig;
 using UnityEngine;
 using HarmonyLib;
+using SLZ.Marrow.Input;
+using SLZ.Marrow.Utilities;
 
 namespace Sst.PancakeMode {
 public class Mod : MelonMod {
@@ -22,49 +25,45 @@ public class Mod : MelonMod {
 
   public override void OnUpdate() {}
 
-  private static bool
-  GetKey(KeyCode key) => Input.GetKey(key) || Input.GetKeyDown(key);
-
   [HarmonyPatch(typeof(OpenController), nameof(OpenController.CacheInputs))]
   class OpenController_CacheInputs_Patch {
     [HarmonyPostfix()]
     internal static void Postfix(OpenController __instance) {
-      if (!_rigManager)
-        return;
+      var cl = __instance.handedness == Handedness.LEFT ? __instance : null;
+      var cr = __instance.handedness == Handedness.RIGHT ? __instance : null;
 
-      if (GetKey(KeyCode.W)) {
-        var cl = _rigManager.ControllerRig.leftController;
-        cl._thumbstickAxis += Vector2.up;
-      }
-      if (GetKey(KeyCode.S)) {
-        var cl = _rigManager.ControllerRig.leftController;
-        cl._thumbstickAxis += Vector2.down;
-      }
-      if (GetKey(KeyCode.A)) {
-        var cl = _rigManager.ControllerRig.leftController;
-        cl._thumbstickAxis += Vector2.left;
-      }
-      if (GetKey(KeyCode.D)) {
-        var cl = _rigManager.ControllerRig.leftController;
-        cl._thumbstickAxis += Vector2.right;
-      }
+      if (cl) {
+        if (Input.GetKey(KeyCode.W))
+          cl._thumbstickAxis += Vector2.up;
+        if (Input.GetKey(KeyCode.S))
+          cl._thumbstickAxis += Vector2.down;
+        if (Input.GetKey(KeyCode.A))
+          cl._thumbstickAxis += Vector2.left;
+        if (Input.GetKey(KeyCode.D))
+          cl._thumbstickAxis += Vector2.right;
 
-      if (GetKey(KeyCode.Space)) {
-        var cr = _rigManager.ControllerRig.rightController;
-        cr._aButton = true;
-        cr._aButtonDown = Input.GetKeyDown(KeyCode.Space);
-        cr._aButtonUp = Input.GetKeyUp(KeyCode.Space);
+        if (Input.GetKey(KeyCode.LeftShift))
+          cl._thumbstick = true;
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+          cl._thumbstickDown = true;
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+          cl._thumbstickUp = true;
       }
 
-      if (Input.GetKey(KeyCode.LeftShift)) {
-        var cl = _rigManager.ControllerRig.leftController;
-        cl._thumbstick = true;
-        cl._thumbstickDown = Input.GetKeyDown(KeyCode.LeftShift);
-        cl._thumbstickUp = Input.GetKeyUp(KeyCode.LeftShift);
-      }
+      if (cr) {
+        if (Input.GetKey(KeyCode.Space))
+          cr._aButton = true;
+        if (Input.GetKeyDown(KeyCode.Space))
+          cr._aButtonDown = true;
+        if (Input.GetKeyUp(KeyCode.Space))
+          cr._aButtonUp = true;
 
-      if (Input.GetKey(KeyCode.LeftControl)) {
-        // _rigManager.ControllerRig._crouchTarget = 0.5f;
+        if (_rigManager) {
+          cr._thumbstickAxis += Input.GetKey(KeyCode.LeftControl) ? Vector2.down
+                                : _rigManager.remapHeptaRig._crouchTarget < 0f
+                                    ? Vector2.up
+                                    : Vector2.zero;
+        }
       }
 
       // if (CurrentMovement == null || !Instance.IsEnabled)
@@ -87,6 +86,81 @@ public class Mod : MelonMod {
       //     CurrentMovementEnumerator = null;
       //   }
       // }
+    }
+  }
+
+  [HarmonyPatch(typeof(Control_GlobalTime), nameof(Control_GlobalTime.PAUSE))]
+  class Control_GlobalTime_PAUSE_Patch {
+    [HarmonyPostfix()]
+    internal static bool Prefix() => false;
+  }
+
+  [HarmonyPatch(typeof(Control_GlobalTime), nameof(Control_GlobalTime.UNPAUSE))]
+  class Control_GlobalTime_UNPAUSE_Patch {
+    [HarmonyPostfix()]
+    internal static bool Prefix() => false;
+  }
+
+  // TODO: Fake XR init which happens in these methods below
+  // MarrowGame.Initialize() -> MarrowGame.TryInitializeXRApi() ->
+  //   XRApi.Initialize() -> XRApi.InitializeXRLoader() -> "XR init failed"
+
+  // [HarmonyPatch(typeof(XRApi), nameof(XRApi.Initialize))]
+  // class XRApi_Initialize_Patch {
+  //   [HarmonyPrefix()]
+  //   internal static void Prefix() { Dbg.Log("XRApi.Initialize Prefix"); }
+
+  //   [HarmonyPostfix()]
+  //   internal static void Postfix() { Dbg.Log("XRApi.Initialize Postfix"); }
+
+  //   [HarmonyFinalizer()]
+  //   internal static void Finalizer(Exception __exception) {
+  //     Dbg.Log(
+  //         $"XRApi.Initialize Finalizer {__exception != null}:
+  //         {__exception}");
+  //   }
+  // }
+
+  // [HarmonyPatch(typeof(XRApi), nameof(XRApi.InitializeXRLoader))]
+  // class XRApi_InitializeXRLoader_Patch {
+  //   [HarmonyPrefix()]
+  //   internal static void Prefix() { MarrowGame.xr.Settings.m_Loaders.Clear();
+  //   }
+  // }
+
+  // [HarmonyPatch(typeof(MarrowGame), nameof(MarrowGame.TryInitializeXRApi))]
+  // class MarrowGame_TryInitializeXRApi_Patch {
+  //   [HarmonyPrefix()]
+  //   internal static void Prefix() {
+  //     Dbg.Log("MarrowGame.TryInitializeXRApi Prefix");
+  //   }
+
+  //   [HarmonyPostfix()]
+  //   internal static void Postfix() {
+  //     Dbg.Log("MarrowGame.TryInitializeXRApi Postfix");
+  //   }
+
+  //   [HarmonyFinalizer()]
+  //   internal static void Finalizer(Exception __exception) {
+  //     Dbg.Log(
+  //         $"MarrowGame.TryInitializeXRApi Finalizer {__exception != null}:
+  //         {__exception}");
+  //     // MarrowGame.xr.Initialize();
+  //   }
+  // }
+
+  [HarmonyPatch(typeof(MarrowGame), nameof(MarrowGame.Initialize))]
+  class MarrowGame_Initialize_Patch {
+    [HarmonyPrefix()]
+    internal static void Prefix() { Dbg.Log("MarrowGame.Initialize Prefix"); }
+
+    [HarmonyPostfix()]
+    internal static void Postfix() { Dbg.Log("MarrowGame.Initialize Postfix"); }
+
+    [HarmonyFinalizer()]
+    internal static void Finalizer(Exception __exception) {
+      Dbg.Log(
+          $"MarrowGame.Initialize Finalizer {__exception != null}: {__exception}");
     }
   }
 }
